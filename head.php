21fbff2083c05fcaf771136f46d45af4761514fd
useless1 <useless@mettingh.de>
2015-04-17 13:48:00 +0200

    Create head.php

---
+++ head.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="keywords" content="open manage it edu">
+  <link rel="stylesheet" href="style.css">
+  <title>Open Manage IT - EDU</title>
+</head>
+<body>
